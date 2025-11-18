5841c73f939c65ee29060df27f05efb684227bcc
DP <dpinoc00@estudiantes.unileon.es>
2025-11-18 18:12:39 +0100

    CSV

--- APS_Solver.py
+++ APS_Solver.py
@@ -1,4 +1,11 @@
-def main():
-    if (i = 1):
-        breakpoint(
-            , kws)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("online_shoppers_intention.csv", usecols=[2,3], names=["ExitRates","Month"], header=0)
+print(data.head())
+
+plt.xlabel("Iteraciones")
+plt.ylabel("Error MSE")
+plt.title("Evolución del error")
+plt.show()
